61adb32755c59a0dbd0f1c6e00a6983c28f2458e
Satya Sundar <satyasundar439@gmail.com>
2026-01-12 06:53:29 +0530

    feat: implement JWT authentication with register and login

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,24 @@
 import express from "express"
 import cors from 'cors'
+import authRoutes from "./routes/authRoutes.js"
+import { protect } from "./middleware/authMiddleware.js"
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
+app.use("/api/v1/auth", authRoutes)
+
 app.get("/health", (req, res) => {
     res.json({status: "OK"})
+})
+
+app.get("/api/v1/protected", protect, (req, res) => {
+    res.json({
+        message: "You are authorized",
+        user: req.user
+    })
 })
 
 export default app
---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,73 @@
+import bcrypt from "bcryptjs"
+import User from "../models/User.js"
+import { generateToken } from "../utils/jwt.js"
+
+
+export const register = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        const existingUser = await User.findOne({ email })
+        if (existingUser) {
+            return res.status(400).json({ message: "Email already exists" })
+        }
+
+        const salt = await bcrypt.genSalt(10)
+        const passwordHash = await bcrypt.hash(password, salt)
+
+        const user = await User.create({
+            name,
+            email,
+            passwordHash
+        })
+
+        const token = generateToken({ userId: user._id })
+
+        res.status(201).json({
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email
+            },
+            token
+        })
+    } catch (error) {
+        res.status(500).json({ message: "REgistration failed" })
+    }
+
+}
+
+export const login = async (req, res) => {
+    try{
+    const { email, password } = req.body
+
+    if(!email || !password){
+        return res.status(400).json({message: "Email and password are required"})
+    }
+
+    const user = await User.findOne({ email }).select("+passwordHash")
+
+    if (!user || !user.passwordHash) {
+        return res.status(400).json({ message: "Invalid Credentials" })
+    }
+
+    const isMatch = await bcrypt.compare(password, user.passwordHash)
+    if (!isMatch) {
+        return res.status(400).json({ message: "Invalid Credentials" })
+    }
+
+    const token = generateToken({ userId: user._id })
+
+    res.json({
+        user: {
+            id: user._id,
+            name: user.name,
+            email: user.email
+        },
+        token
+    })
+} catch(error){
+    console.error("Login Error", error)
+    res.status(500).json({message: "Login failed"})
+}
+}
